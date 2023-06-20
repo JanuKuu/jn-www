ec6833c3cddd3363f1144b305819ee685038c4fb
JahNoah <jahnoah@protonmail.com>
2023-06-20 16:41:14 -0400

    3d component template code

--- my-site/src/comps/floatingDino.vue
+++ my-site/src/comps/floatingDino.vue
@@ -2,26 +2,54 @@
     <div id="container" class="container"></div>
 </template>
 
-<script>
 
-import * as THREE from 'three';
+<script>
+import * as Three from 'three'
 
 export default {
+  name: 'ThreeTest',
+  data() {
+    return {
+      camera: null,
+      scene: null,
+      renderer: null,
+      mesh: null
+    }
+  },
+  methods: {
+    init: function() {
+        let container = document.getElementById('container');
 
-    
+        this.camera = new Three.PerspectiveCamera(70, container.clientWidth/container.clientHeight, 0.01, 10);
+        this.camera.position.z = 1;
+
+        this.scene = new Three.Scene();
+
+        let geometry = new Three.BoxGeometry(0.2, 0.2, 0.2);
+        let material = new Three.MeshNormalMaterial();
+
+        this.mesh = new Three.Mesh(geometry, material);
+        this.scene.add(this.mesh);
+
+        this.renderer = new Three.WebGLRenderer({antialias: true});
+        this.renderer.setSize(container.clientWidth, container.clientHeight);
+        container.appendChild(this.renderer.domElement);
+
+    },
+    animate: function() {
+        requestAnimationFrame(this.animate);
+        this.mesh.rotation.x += 0.01;
+        this.mesh.rotation.y += 0.02;
+        this.renderer.render(this.scene, this.camera);
+    }
+  },
+  mounted() {
+      this.init();
+      this.animate();
+  }
 }
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGL1Renderer({
-    canvas: document.querySelector('#CEO3d'),
-});
-
-renderer.setPixelRatio( window.devicePixelRatio );
-renderer.setSize( window.innerWidth, window.innerHeight);
-camera.getWorldPosition.setZ(30);
-
-renderer.render(scene, camera);
-
-
-
 </script>
+
+<style scoped>
+    /* TODO give container a size. */
+</style>
